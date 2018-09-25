2b35f6cf92802d46bcc6cb926cd4847fc5af584e
varung97 <varung97@gmail.com>
2018-09-25 11:59:38 +0800

    Add correlate

--- machine_learning/feature_extraction.py
+++ machine_learning/feature_extraction.py
@@ -4,6 +4,8 @@ import numpy as np
 def extract_feature_over_each_sensor(segment, feature_extractor_over_sensor):
     """A feature extractor over sensor is a function that accepts a
     sensor_data.SensorData and returns a list of features.
+
+    Such an extractor is applied to each sensor to obtain a list of features.
     """
     return np.concatenate([
         feature_extractor_over_sensor(sensor_data)
@@ -14,6 +16,9 @@ def extract_feature_over_each_sensor(segment, feature_extractor_over_sensor):
 def extract_feature_over_all_axes(segment, feature_extractor_over_axis):
     """A feature extractor over axis is a function that accepts a 1D array
     and returns a list of features.
+
+    Such an extractor is applied to each axis in each sensor to obtain a list
+    of features.
     """
     return extract_feature_over_each_sensor(
         segment,
@@ -34,8 +39,21 @@ def stdev(segment):
     return extract_feature_over_all_axes(segment, np.std)
 
 
-# def correlate(segment):
+def correlate(segment):
+    def correlate_over_sensor(sensor_data):
+        def correlate_over_triaxial_data(triaxial_data):
+            # We want Cxy, Cxz and Cyz
+            # These are present at locations (0, 1), (0, 2) and (1, 2) respectively
+            # To obtain these values using numpy, we need to provide a list of all
+            # the row indices and a list of all the column indices
+            return np.corrcoef(np.transpose(triaxial_data))[[0, 0, 1], [1, 2, 2]]
 
+        return np.concatenate([
+            correlate_over_triaxial_data(sensor_data.acc),
+            correlate_over_triaxial_data(sensor_data.gyro)
+        ])
+
+    return extract_feature_over_each_sensor(segment, correlate_over_sensor)
 
 
 def extract_features(segment, feature_extractors):
@@ -57,4 +75,4 @@ if __name__ == '__main__':
     labels = [random.randrange(1, 12) for i in range(len(collector.move_start_indices))]
 
     segments = collector.segment(labels)
-    print(extract_features(segments[0], [mean, stdev]))
+    print(extract_features(segments[0], [mean, stdev, correlate]))
