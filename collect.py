eac278a2a420dbb12b762ec17b81d580d241baae
varung97 <varung97@gmail.com>
2018-09-26 18:19:06 +0800

    Some cleanup

--- collect.py
+++ collect.py
@@ -1,8 +1,7 @@
+import _thread
 from comms import read_data
 from machine_learning import data_collection, sensor_data
 from pynput.keyboard import Listener, Key, KeyCode
-import sys
-import _thread
 
 
 def register_kbd_listeners(on_move, on_quit):
@@ -18,19 +17,20 @@ def register_kbd_listeners(on_move, on_quit):
 
 
 if __name__ == '__main__':
-    data_collection = data_collection.DataCollection('test.pb')
+    try:
+        data_collection = data_collection.DataCollection('test.pb')
 
-    def on_quit():
-        data_collection.save()
-        _thread.interrupt_main()
+        def on_quit():
+            data_collection.save()
+            _thread.interrupt_main()
 
-    register_kbd_listeners(data_collection.next_move, on_quit)
+        register_kbd_listeners(data_collection.next_move, on_quit)
 
-    while True:
-        pass
+        for packet in read_data.read_data():
+            sensor1_datum = sensor_data.SensorDatum(packet[0:3], packet[3:6])
+            sensor2_datum = sensor_data.SensorDatum(packet[6:9], packet[9:12])
 
-    # for packet in read_data.read_data():
-    #     sensor1_datum = sensor_data.SensorDatum(packet[0:3], packet[3:6])
-    #     sensor2_datum = sensor_data.SensorDatum(packet[6:9], packet[9:12])
-    #
-    #     data_collection.process([sensor1_datum, sensor2_datum])
+            data_collection.process([sensor1_datum, sensor2_datum])
+
+    except KeyboardInterrupt:
+        print('Done!')
