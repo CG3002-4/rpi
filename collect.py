1c0e7a47507ed42002fc260e20c7c54c40112b67
varung97 <varung97@gmail.com>
2018-09-27 15:51:40 +0800

    Remove keyboard code

--- collect.py
+++ collect.py
@@ -1,30 +1,30 @@
 import _thread
 from comms import read_data
 from machine_learning import data_collection, sensor_data
-from pynput.keyboard import Listener, Key, KeyCode
+# from pynput.keyboard import Listener, Key, KeyCode
 
 
-def register_kbd_listeners(on_move, on_quit):
-    def on_press(key):
-        if key == Key.space:
-            print('Move')
-            on_move()
-        elif key == KeyCode.from_char('q'):
-            print('Quit')
-            on_quit()
-
-    Listener(on_press=on_press).start()
+# def register_kbd_listeners(on_move, on_quit):
+#     def on_press(key):
+#         if key == Key.space:
+#             print('Move')
+#             on_move()
+#         elif key == KeyCode.from_char('q'):
+#             print('Quit')
+#             on_quit()
+#
+#     Listener(on_press=on_press).start()
 
 
 if __name__ == '__main__':
+    data_collection = data_collection.DataCollection('test.pb')
+
     try:
-        data_collection = data_collection.DataCollection('test.pb')
-
-        def on_quit():
-            data_collection.save()
-            _thread.interrupt_main()
-
-        register_kbd_listeners(data_collection.next_move, on_quit)
+        # def on_quit():
+        #     data_collection.save()
+        #     _thread.interrupt_main()
+        #
+        # register_kbd_listeners(data_collection.next_move, on_quit)
 
         for packet in read_data.read_data():
             sensor1_datum = sensor_data.SensorDatum(packet[0:3], packet[3:6])
@@ -33,4 +33,5 @@ if __name__ == '__main__':
             data_collection.process([sensor1_datum, sensor2_datum])
 
     except KeyboardInterrupt:
+        data_collection.save()
         print('Done!')
