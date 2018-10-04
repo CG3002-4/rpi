3ae0efe0717866971dac2c9d109e4f374834949a
varung97 <varung97@gmail.com>
2018-10-04 17:01:53 +0800

    Add training and pipeline code

---
+++ machine_learning/pipeline.py
@@ -0,0 +1,51 @@
+import sys
+import numpy as np
+import pickle
+from machine_learning.data_collection import DataCollection
+from machine_learning import feature_extraction
+from machine_learning import train
+
+
+FEATURE_EXTRACTORS = [feature_extraction.mean, feature_extraction.stdev, feature_extraction.correlate]
+
+
+def pipeline(experiment_names):
+    assert len(experiment_names) > 0
+
+    segments = []
+
+    for experiment_name in experiment_names:
+        data_collection = DataCollection(experiment_name)
+        data_collection.load()
+        segments.extend(data_collection.segment())
+
+    features = feature_extraction.extract_features(segments, FEATURE_EXTRACTORS)
+    labels = np.array([segment.label for segment in segments])
+
+    return features, labels
+
+
+def train_pipeline(experiment_names, model_filename):
+    features, labels = pipeline(experiment_names)
+
+    model = train.train(X=features, y=labels)
+
+    with open(model_filename, 'wb') as f:
+        pickle.dump(model, f)
+
+
+def cross_validate_pipeline(experiment_names):
+    features, labels = pipeline(experiment_names)
+
+    train.cross_validate(X=features, y=labels)
+
+
+if __name__ == '__main__':
+    pipeline_type = sys.argv[1]
+
+    if pipeline_type == 'train':
+        train_pipeline(sys.argv[3:], sys.argv[2])
+    elif pipeline_type == 'cross':
+        cross_validate_pipeline(sys.argv[2:])
+    else:
+        print('Unrecognized pipeline: ' + pipeline_type)
