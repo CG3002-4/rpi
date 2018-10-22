718326c94ffbf7898aa0316ab3a04f9688176a35
varung97 <varung97@gmail.com>
2018-10-22 21:41:23 +0800

    Add server code

--- clientconnect.py
+++ clientconnect.py
@@ -18,20 +18,22 @@ secret_key = '1234567887654321'
 BS = 16
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
 
+
 def handshake():
     ser = serial.Serial("/dev/ttyAMA0", 115200)
     ser.flushInput()
-    
+
     time.sleep(5)
     ser.write("4".encode('utf8'))
     ack = ser.read(1)
-    
+
     if (ack[0] == 6):
         ser.write("3".encode('utf8'))
     else:
         assert False, "Handshaking failed"
-        
+
     return ser
+
 
 def read_and_analyse_data(ser):
     while True:
@@ -42,7 +44,6 @@ def read_and_analyse_data(ser):
             checksum ^= byte
 
         if checksum == packet[DATA_SIZE]:
-            print('Checksum match')
             contents = np.array([struct.unpack('>h', packet[i: i + 2])[0] for i in range(0, 32, 2)])
             yield contents / 100
         else:
@@ -50,22 +51,25 @@ def read_and_analyse_data(ser):
             print(packet)
             print()
 
+
 def encryptText(msg, Key):
     iv = Random.new().read(AES.block_size)
-    secret_key = bytes(str(Key), encoding = "utf8")
-    cipher = AES.new(secret_key,AES.MODE_CBC,iv)
+    secret_key = bytes(str(Key), encoding="utf8")
+    cipher = AES.new(secret_key, AES.MODE_CBC, iv)
     encryptedText = cipher.encrypt(pad(msg))
     encodedText = base64.b64encode(iv + encryptedText)
 
     return encodedText
 
+
 def create_socket(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
     print("Socket connected")
-    
+
     return s
-    
+
+
 def switch_action(action_num):
     switcher = {
         1: "wipers",
@@ -75,20 +79,20 @@ def switch_action(action_num):
         5: "turnclap",
         11: "logout"
     }
-    
+
     return switcher.get(action_num, "Invalid move")
-        
+
 
 def send_data(s, action_num, voltage, current, power, cumpower):
     action = switch_action(action_num)
     results = "#" + action + "|" + str(voltage) + "|" + str(current) + "|" + str(power) + "|" + str(cumpower)
     encodedResults = encryptText(results, secret_key)
     s.sendall(encodedResults)
-        
+
     if action == "logout":
         return -1
+
 
 def logout(s):
     print("Bye!")
     s.close()
-        
--- rpi_realtime.py
+++ rpi_realtime.py
@@ -1,28 +1,30 @@
 import sys
 import pickle
 import numpy as np
+from recv_data import recv_data
 from data_collection import NUM_SENSORS
 from sensor_data import SensorDatum, sensor_datums_to_sensor_data
 from segment import Segment, SEGMENT_SIZE, SEGMENT_OVERLAP
 from preprocess import preprocess_segment
 from feature_extraction import extract_features_over_segment
-from pipeline import NOISE_FILTERS, FEATURE_EXTRACTORS
-from clientconnect import handshake, read_and_analyse_data
+import clientconnect
 import time
+
 
 class SegmentPredictor:
     def __init__(self, model_file):
         with open(model_file, 'rb') as f:
             self.model = pickle.load(f)
 
-        self.data = np.empty((0, NUM_SENSORS))
+        self.data = np.empty((0, 12))
         self.portion_of_segment_complete = 0
 
     def process(self, sensors_datum):
-        assert len(sensors_datum) == NUM_SENSORS
+        assert len(sensors_datum) == 12
 
         if len(self.data) < SEGMENT_SIZE:
             self.data = np.vstack([self.data, sensors_datum])
+
         else:
             # len(self.data) has to be equal to segment size
             self.data = np.vstack([self.data[1:], sensors_datum])
@@ -36,25 +38,30 @@ class SegmentPredictor:
             return self.make_prediction()
 
     def make_prediction(self):
-        # TODO: Takes too much time
-        sensors_data = np.apply_along_axis(func1d=sensor_datums_to_sensor_data, arr=self.data, axis=0)
-        segment = Segment(sensors_data, None)
-        segment = preprocess_segment(segment, NOISE_FILTERS)
-        features = extract_features_over_segment(segment, FEATURE_EXTRACTORS)
-        return self.model.predict_proba(features)[0]
+        segment = Segment(self.data, None)
+        segment = preprocess_segment(segment)
+        features = extract_features_over_segment(segment)
+        features = np.nan_to_num(features)
+
+        return self.model.predict_proba(features.reshape(1, -1))[0]
 
 
-NUM_PREDS_TO_KEEP = 6
-NUM_MOVES = 5
-PREDICTION_THRESHOLD = 0.8
+NUM_PREDS_TO_KEEP = 5
+NUM_MOVES = 6
+PREDICTION_THRESHOLD = 0.7
+TIME_TO_DISCARD = 1
 
 
 class Predictor:
     def __init__(self, model_file):
         self.segment_predictor = SegmentPredictor(model_file)
         self.predictions = np.empty((0, NUM_MOVES))
+        self.prevPredictionTime = time.time()
 
     def process(self, sensors_datum):
+        if time.time() - self.predictionTime < TIME_TO_DISCARD:
+            return
+
         segment_prediction = self.segment_predictor.process(sensors_datum)
 
         if segment_prediction is not None:
@@ -74,25 +81,34 @@ class Predictor:
         print('Probabilities: ' + str(normalized_probs))
 
         if max(normalized_probs) > PREDICTION_THRESHOLD:
-            print(np.argmax(normalized_probs) + 1)
-            self.predictions = np.empty((0, NUM_MOVES))
-            return np.argmax(normalized_probs) + 1
+            prediction = np.argmax(normalized_probs)
+            self.prevPredictionTime = time.time()
+
+            return prediction
 
 
 if __name__ == '__main__':
-    ser = handshake()
-
     np.set_printoptions(suppress=True)
-    
+    np.seterr(divide='ignore', invalid='ignore')
+
+    socket = clientconnect.create_socket(sys.argv[2], sys.argv[3])
     predictor = Predictor(model_file=sys.argv[1] + '.pb')
 
     print('Loaded model')
 
-    for unpacked_data in read_and_analyse_data(ser):
-        sensor1_datum = SensorDatum(
-            unpacked_data[0:3], unpacked_data[3:6])
-        sensor2_datum = SensorDatum(
-            unpacked_data[6:9], unpacked_data[9:12])
+    energy = 0
+    prev_time = None
 
-        # Need to call server comm code here
-        predictor.process([sensor1_datum, sensor2_datum])
+    for unpacked_data in recv_data():
+        voltage = unpacked_data[12]
+        current = unpacked_data[13]
+        power = voltage * current
+
+        if prev_time is not None:
+            energy += power * (time.time() - prev_time)
+        prev_time = time.time()
+
+        prediction = predictor.process(unpacked_data[:12])
+
+        if prediction is not None:
+            clientconnect.send_data(socket, prediction, voltage, current, power, energy)
