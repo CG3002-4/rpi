f2fc892d99e4bba8ac44c3d3e6d34a1a83cb987e
varung97 <varung97@gmail.com>
2018-10-23 10:47:17 +0800

    Change rpi code to account for sending as ASCII

--- recv_data.py
+++ recv_data.py
@@ -5,39 +5,41 @@ import struct
 import numpy as np
 import time
 
-DATA_SIZE = 28
+NUM_DATUM = 14
 HANDSHAKING = False
+
 
 def recv_data():
     ser = serial.Serial("/dev/ttyAMA0", 115200)
     ser.flushInput()
+
     if HANDSHAKING:
         time.sleep(5)
         ser.write("4".encode('utf8'))
         ack = ser.read(1)
-    
+
         if (ack[0] == 6):
             ser.write("3".encode('utf8'))
             print("Handshaking success")
         else:
             assert False, "Handshaking failed"
-    
+
     print('Looking for data')
     while True:
-        packet = ser.read(DATA_SIZE + 1)
+        packet = ser.readline()
+        print(packet)
+
+        data = np.array(packet.split(',')).astype(int)
+        print(data)
+        data_as_bytes = b''.join([struct.pack('>h', datum) for datum in data[:-1]])
+        print(data_as_bytes)
 
         checksum = 0
-        for byte in packet[:DATA_SIZE]:
+        for byte in data_as_bytes:
             checksum ^= byte
 
-        if checksum != packet[DATA_SIZE]:
+        if checksum != data[NUM_DATUM]:
             print('Checksums didn\'t match')
-            print(packet)
             print()
 
-        contents = np.array([struct.unpack('>h', packet[i: i + 2])[0]
-                             for i in range(0, DATA_SIZE, 2)])
-        yield contents / 100
-    
-
-    
+        yield data / 100
